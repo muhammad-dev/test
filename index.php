cc9fad5e7b951c51292e18d23f75c58146adb4b5
muhammad-dev <ali@eforte.net>
2019-10-04 12:23:19 +0500

    git ignore testing

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once('googlePlaces.php');
+
+$apiKey       = 'Your Google Places API Key';
+$googlePlaces = new googlePlaces($apiKey);
+
+// Set the longitude and the latitude of the location you want to search near for places
+$latitude   = '-33.8804166';
+$longitude = '151.2107662';
+$googlePlaces->setLocation($latitude . ',' . $longitude);
+
+$googlePlaces->setRadius(5000);
+$results = $googlePlaces->search(); //
+
+
+?>
